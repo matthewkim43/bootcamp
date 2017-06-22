f36f1c78c0214b5c042ffed0c9597dc3fe46088d
Matthew S Kim <matthewkim43@gmail.com>
2017-06-22 10:36:11 -0700

    Added bootcamp_utils

---
+++ bootcamp_utils.py
@@ -0,0 +1,18 @@
+"""
+Commonly used packages,functions,etc.
+""""
+
+import numpy as py
+import matplotlib.pyplt as pyplt
+import seaborn as sns
+
+sns.set()
+
+def ecdf(data):
+    """Computes and plots ECDF"""
+
+    # Creating x and y values
+    x = np.sort(data)
+    y = np.arange(1,len(data)+1)/len(data)
+
+    return x, y
